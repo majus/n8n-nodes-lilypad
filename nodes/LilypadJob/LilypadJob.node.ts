9149478ff9aa6888077ab81315d68145f5ba651e
Denis Perov <denis.perov@gmail.com>
2024-05-28 19:12:56 +0300

    Actually implement LilypadJob node behavior

--- nodes/LilypadJob/LilypadJob.node.ts
+++ nodes/LilypadJob/LilypadJob.node.ts
@@ -5,6 +5,8 @@ import {
 	INodeTypeDescription,
 	NodeOperationError,
 } from 'n8n-workflow';
+import { isObject } from 'lodash';
+import fetch from 'node-fetch';
 
 export class LilypadJob implements INodeType {
 	description: INodeTypeDescription = {
@@ -13,18 +15,25 @@ export class LilypadJob implements INodeType {
 		group: ['transform', 'output'],
 		version: 1,
 		description: 'Execute Lilypad job',
-		icon: 'file:lilypad.png',
+		icon: 'file:Lilypad.svg',
 		defaults: {
 			name: 'Lilypad Job',
 		},
 		inputs: ['main'],
 		outputs: ['main'],
+		credentials: [
+			{
+				name: 'lilypadCredentialsApi',
+				required: true,
+			},
+		],
 		properties: [
 			{
 				displayName: 'Module',
 				name: 'module',
 				type: 'string',
-				default: '',
+				required: true,
+				default: 'ollama-pipeline:llama3-8b-lilypad1',
 			},
 			{
 				displayName: 'Inputs',
@@ -43,6 +52,7 @@ export class LilypadJob implements INodeType {
 								displayName: 'Name',
 								name: 'name',
 								type: 'string',
+								required: true,
 								default: '',
 								description: 'Custom input name',
 							},
@@ -50,6 +60,9 @@ export class LilypadJob implements INodeType {
 								displayName: 'Value',
 								name: 'value',
 								type: 'string',
+								typeOptions: {
+									rows: 4,
+								},
 								default: '',
 								description: 'Custom input value',
 							},
@@ -61,36 +74,51 @@ export class LilypadJob implements INodeType {
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const outputs = [];
 		const items = this.getInputData();
-		let item: INodeExecutionData;
-		let myString: string;
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
-		// (This could be a different value for each item in case it contains an expression)
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+		// Variables could have different value for each item in case they contain an expression
+		for (let index = 0; index < items.length; index++) {
 			try {
-				myString = this.getNodeParameter('myString', itemIndex, '') as string;
-				item = items[itemIndex];
-				item.json['myString'] = myString;
+				const module = this.getNodeParameter('module', index, '') as string;
+				const inputs = this.getNodeParameter('inputs', index) as { items: object[] };
+				const { key } = await this.getCredentials('lilypadCredentialsApi') as { key: string };
+				const response = await fetch('http://js-cli-wrapper.lilypad.tech', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						pk: key,
+						module,
+						// @ts-ignore
+						inputs: inputs.items?.map(({ name, value }) => {
+							//TODO: Escape instad of just removing
+							const sanitized = value.replace(/'/g, '');
+							return `-i ${name}='${sanitized}'`;
+						}).join(' '),
+					}),
+				});
+				const json = await response.json();
+				if (json.error) {
+					const error = isObject(json.error) ? JSON.stringify(json.error) : json.error;
+					throw new NodeOperationError(this.getNode(), error);
+				}
+				outputs.push({ json });
 			} catch (error) {
-				// This node should never fail but we want to showcase how
-				// to handle errors.
 				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+					const { json } = this.getInputData(index)[0];
+					items.push({ json, error, pairedItem: index });
 				} else {
 					// Adding `itemIndex` allows other workflows to handle this error
 					if (error.context) {
-						// If the error thrown already contains the context property,
-						// only append the itemIndex
-						error.context.itemIndex = itemIndex;
+						// If the error thrown already contains the context property, only append the itemIndex
+						error.context.itemIndex = index;
 						throw error;
 					}
 					throw new NodeOperationError(this.getNode(), error, {
-						itemIndex,
+						itemIndex: index,
 					});
 				}
 			}
 		}
-		return this.prepareOutputData(items);
+		return this.prepareOutputData(outputs);
 	}
 }
